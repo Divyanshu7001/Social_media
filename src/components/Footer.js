83f1e244379b770908a0216ebd648ea541809507
Divyanshu7031 <divyanshuverma195@gmail.com>
2025-04-24 17:33:22 +0530

    Made Footer Responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
   const { popup } = useContext(Context)
   return (
     <div
-      className={popup ? " bg-black bg-opacity-50 py-8 px-8 relative" : "text-white py-8 px-8 relative bg-primary"}
+      className={popup ? " bg-black bg-opacity-50 py-8 px-8 relative" : "text-white xss:py-4 xss:px-3 xs:py-8 xs:px-8 relative bg-primary"}
       // style={{ backgroundColor: "rgb(0, 0, 255)" }}
     >
       {/* Gray Overlay */}
@@ -20,20 +20,19 @@ function Footer() {
 
       <footer className="relative max-w-[90%] mx-auto flex flex-col items-center my-8">
         {/* Newsletter Section */}
-        <div className="w-full text-center mb-16 py-6 border-b border-gray-50 border-opacity-20 grid grid-cols-5">
-          <div className="col-span-2 text-left ml-20 space-y-2">
-            <p className="p">News Letter</p>
-            <h3 className="head">Get Monthly Updates</h3>
+        <div className="w-full text-center xss:mb-4 md:mb-6 lg:mb-16 py-6 border-b border-gray-50 border-opacity-20 md:grid md:grid-cols-5 lg:grid-cols-5">
+          <div className="col-span-2 text-center md:text-left lg:ml-12 space-y-2">
+            <p className="p">News Letter</p> 
+            <h3 className="head text-3xl lg:text-4xl">Get Monthly Updates</h3>
           </div>
-          <div className=" col-span-3 max-w-3xl">
-            <div className="flex justify-between border-b border-opacity-80 border-gray-300 pb-2">
-              <h3 className="mt-4 p ml-2">
+          <div className="col-span-3 max-w-4xl">
+            <div className="flex justify-between border-b border-opacity-80 text-left border-gray-300 pb-2">
+              <h5 className="mt-4 p">
                 Enter your Email address and get updates directly to your email
-              </h3>
-              <img alt="arrow" className="mr-12" src={epback} style={{
+              </h5>
+              <img alt="arrow" className="xss:mt-7 md:mt-5 lg:mt-[12px] mt-[8px] md:mr-12" src={epback} style={{
                 height: "28px",
                 width: "72px",
-                marginTop: "8px"
               }} >
               </img>
             </div>
@@ -43,7 +42,7 @@ function Footer() {
         {/* Links Section */}
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full md:text-left mb-8">
           {/* Social Media Section */}
-          <div className="ml-8 mt-16 col-span-1">
+          <div className="lg:ml-8 mt-16 col-span-1 xss:mx-auto xs:mx-0">
             <p className="font-semibold mb-4 text-white text-xl">Follow Us On</p>
             <div className="flex space-x-6">
               <a
@@ -70,9 +69,9 @@ function Footer() {
             </div>
           </div>
 
-          <div className="ml-8 col-span-3 max-w-4xl grid grid-cols-3">
-            <div className="text-center md:text-left">
-              <h4 className="font-semibold text-lg mb-4">Company</h4>
+          <div className="lg:ml-8 col-span-3 max-w-4xl grid grid-cols-3">
+            <div className="text-center xs:text-left">
+              <h4 className="font-semibold text-xl mb-4">Company</h4>
               <ul className="space-y-4">
                 <li>
                   <Link to="/about">About Us</Link>
@@ -87,8 +86,8 @@ function Footer() {
             </div>
 
             {/* Resources Section */}
-            <div className="text-center md:text-left">
-              <h4 className="font-semibold text-lg mb-4">Resources</h4>
+            <div className="text-center xs:text-left">
+              <h4 className="font-semibold text-xl mb-4">Resources</h4>
               <ul className="space-y-4">
                 <li>
                   <Link to="/institutions">Institutions</Link>
@@ -103,8 +102,8 @@ function Footer() {
             </div>
 
             {/* Support Section */}
-            <div className="text-center md:text-left">
-              <h4 className="font-semibold text-lg mb-4">Support</h4>
+            <div className="text-center xs:text-left">
+              <h4 className="font-semibold text-xl mb-4">Support</h4>
               <ul className="space-y-4">
                 <li>
                   <Link to="/guidelines">Guidelines</Link>
