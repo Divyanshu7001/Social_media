9777a09143a8271e99f61e12d615a29f543cbf9b
Deepan7010 <deepanchakkaravarthi49@gmail.com>
2025-05-03 09:11:16 +0530

    Color change

--- src/components/SavedItems.js
+++ src/components/SavedItems.js
@@ -73,8 +73,8 @@ const SavedItems = () => {
           <h2 className="font-bold mb-[1vw] text-lg ">SAVED ITEMS</h2>
 
           <div className='flex gap-5 w-full justify-center rounded'>
-            <button className={`px-3 py-1 mb-2 w-1/3 font-bold border-b-2 text-2xl ${btn === "article" ? "border-gray-500 text-gray-500" : "border-primary  text-primary"}`} onClick={() => setBtn("article")}>Articles</button>
-            <button className={`px-3 py-1 mb-2 w-1/3 font-bold border-b-2 text-2xl ${btn === "post" ? "border-gray-500  text-gray-500" : "border-primary text-primary "}`} onClick={() => setBtn("post")}>Post</button>
+            <button className={`px-3 py-1 mb-2 w-1/3 font-bold border-b-2 text-2xl ${btn === "article" ? "border-primary  text-primary" : "border-gray-500 text-gray-500"}`} onClick={() => setBtn("article")}>Articles</button>
+            <button className={`px-3 py-1 mb-2 w-1/3 font-bold border-b-2 text-2xl ${btn === "post" ? " border-primary text-primary " : "border-gray-500  text-gray-500"}`} onClick={() => setBtn("post")}>Post</button>
           </div>
 
           {btn === "article" && (articles.length > 0 ? articles.map((art) => (
